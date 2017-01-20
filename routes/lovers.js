212e76e7b0450d2605a2de112acfbcb6a98bbfa7
Ruchir Patel <ruchir594@gmail.com>
2017-01-20 11:30:20 +0530

    playing around

--- routes/lovers.js
+++ routes/lovers.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', love: 'kiss' });
+  res.render('dang', { title: 'Bang', love: 'Kiss' });
 });
 
 module.exports = router;
