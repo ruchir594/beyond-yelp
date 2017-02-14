67ce79cb953fb8db6f76fc00afacd1b40e800a7f
Ruchir Patel <ruchir594@gmail.com>
2017-02-14 15:26:41 +0530

    improved efficiency in redesigned UI- not calling CSS unnecessarily

--- routes/me.js
+++ routes/me.js
@@ -55,6 +55,48 @@ router.post('/', function(req, res){
     //res.render('update', {user:req.user, fuser:'', message:'Succesfully'});
 })
 
+/* receiving the form and updating the information */
+router.post('/pro', function(req, res){
+    var db = req.db;
+    var collection = db.get('users');
+    //console.log(req.user);
+    console.log(req.body.time);
+    var obj_update = {};
+    if (req.body.age) obj_update.age = req.body.age;
+    if (req.body.location) {
+        obj_update.location = req.body.location;
+        geocoder.geocode(req.body.location, function ( err, data ) {
+          obj_update.coord = [data.results[0].geometry.location.lat, data.results[0].geometry.location.lng];
+          collection.update({oauthID: req.user.oauthID},
+          {
+              $set: obj_update
+          });
+        });
+    }
+    if (req.body.hedu) obj_update.education = req.body.hedu;
+    if (req.body.fostudy) obj_update.major = req.body.fostudy;
+    obj_update.lastUpdated = Date.now();
+    console.log(obj_update);
+    collection.update({oauthID: req.user.oauthID},
+    {
+        $set: obj_update,
+        $push: { waytrn1: req.body.waytrn1 }
+    }, function(err, result) {
+        if (err) {
+            console.log(err);
+            res.send("There was a problem updating the information in the database.");
+        }
+        else {
+            console.log(result);
+            collection.find({oauthID: req.user.oauthID}, {}, function(e, docs){
+                res.render('me_pro', {user:docs[0], fuser:'', message:'Succesfully updated your Profile! :) '});
+            });
+        }
+    });
+    //console.log(update_user);
+    //res.render('update', {user:req.user, fuser:'', message:'Succesfully'});
+})
+
 /*POST on update account*/
 router.get('/update', function(req, res){
     res.render('update', {user:req.user, fuser:'', message:''});
